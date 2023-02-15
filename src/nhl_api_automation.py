88372d1d0700de21023974df80bdb518dca08b22
Martin Miller <martinmiller@Martins-MacBook-Pro.local>
2023-02-15 15:12:25 -0500

    Correct spelling of Montréal Canadiens added where appropriate

--- src/nhl_api_automation.py
+++ src/nhl_api_automation.py
@@ -6,33 +6,36 @@ Created on Fri Jan 27 23:31:39 2023
 """
 
 import requests
-import pandas as pd
 from datetime import datetime
 
+import sqlite3
+
 from nhl_database.queries import epochtime
+from nhl_database.nhl_data_models import database, Games
 
 base_url = "https://statsapi.web.nhl.com/api/v1/schedule"
 
-#API call variable
-game_date = "2023-01-07"
+# API call variable
+game_date = "2023-02-11"
 
 r = requests.get(url=base_url+"?date="+game_date)
 data = r.json()
 
 games = data["dates"][0]["games"]
 
-#NB: Schedule provides all dates in the Zulu time, so some dates are
-#"in the future" but not really.
+# NB: Schedule provides all dates in the Zulu time, so some dates are
+# "in the future" but not really.
 
-#As with the NBA data, more advanced information can be obtained using the APO
-#and this file will provide a basis. The NHL API is far easier to use
-#than the NBA API, and has been used by other people for far more advanced
-#projects. Here this is only for scores.
+# As with the NBA data, more advanced information can be obtained using the APO
+# and this file will provide a basis. The NHL API is far easier to use
+# than the NBA API, and has been used by other people for far more advanced
+# projects. Here this is only for scores.
 
-game_list=[]
+game_list = []
 for g in games:
     game_dict = {}
-    r_game = requests.get("https://statsapi.web.nhl.com/api/v1/game/"+str(g["gamePk"])+"/linescore")
+    r_game = requests.get(
+        "https://statsapi.web.nhl.com/api/v1/game/"+str(g["gamePk"])+"/linescore")
     game_data = r_game.json()
     game_dict["away_team_name"] = game_data["teams"]["away"]["team"]["name"]
     game_dict["away_g"] = game_data["teams"]["away"]["goals"]
@@ -52,4 +55,8 @@ for g in games:
     game_dict["game_datetime"] = datetime.fromisoformat(game_date)
     game_dict["datetime"] = epochtime(game_dict["game_datetime"])
     game_list.append(game_dict)
+
+for z in game_list:
+    #Quick 
+    Games.update(visitor_g = z["away_g"], home_g = z["home_g"]).where((Games.game_date == z["game_date"]) & (Games.visitor == z["away_team_name"]) & (Games.home == z["home_team_name"])).execute()
     
