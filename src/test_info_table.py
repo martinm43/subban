6bcdf3627a3f7584b557daf422b9625ced9f70cd
MA Miller <miller.m.43@gmail.com>
2023-04-28 17:20:02 -0400

    Saving working configuration with working unit tests

--- src/test_info_table.py
+++ src/test_info_table.py
@@ -6,11 +6,14 @@ import pytest
 from info_table import info_table_data
 from datetime import datetime
 
-#Test setup values
+
+#WARNING: as of now only works for alphabetically sorted order!!
+
+#variables
 season_year = 2023
 start_datetime = datetime(season_year-1,10,1)
 end_datetime = datetime(season_year,4,20)
-team_index = 31
+team_index = 15
 ovr_loc = 6
 
 
@@ -29,22 +32,22 @@ def test_team_tuple(itd):
     tm_fn = itd
     tm_tuple = tm_fn(season_year,start_datetime,end_datetime)
     tm_abbrev = tm_tuple[team_index][0]
-    assert(tm_abbrev=="WPG")
+    assert(tm_abbrev=="MTL")
 
 def test_wins(itd):
     tm_fn = itd
     tm_tuple = tm_fn(season_year,start_datetime,end_datetime)
     tm_abbrev = tm_tuple[team_index][ovr_loc]
-    assert(tm_abbrev[0:2]=="46")
+    assert(tm_abbrev[0:2]=="31")
 
 def test_losses(itd):
     tm_fn = itd
     tm_tuple = tm_fn(season_year,start_datetime,end_datetime)
     tm_abbrev = tm_tuple[team_index][ovr_loc]
-    assert(tm_abbrev[3:5]=="35") 
+    assert(tm_abbrev[3:5]=="45") 
     
 def test_OTL_losses(itd):
     tm_fn = itd
     tm_tuple = tm_fn(season_year,start_datetime,end_datetime)
     tm_abbrev = tm_tuple[team_index][ovr_loc]
-    assert(tm_abbrev[6:8]=="1") 
+    assert(tm_abbrev[6:8]=="6") 
