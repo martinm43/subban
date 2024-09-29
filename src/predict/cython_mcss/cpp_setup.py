419a92b5ba7d5672df2930dcb08d250fa8e62f73
MA Miller <miller.m.43@gmail.com>
2024-09-28 20:09:05 -0400

    Catalina working build

--- src/predict/cython_mcss/cpp_setup.py
+++ src/predict/cython_mcss/cpp_setup.py
@@ -22,6 +22,8 @@ import os
 ext = Extension(
     "mcss_ext2",
     sources=["mcss_ext2.pyx", "mcss.cpp"],
+    library_dirs=["/usr/local/lib"],
+    include_dirs=["/usr/local/include"],
     extra_compile_args=["-std=c++11"],
 
 )
--- src/predict/cython_mcss/ext_build.sh
+++ src/predict/cython_mcss/ext_build.sh
@@ -2,4 +2,5 @@
 # Delete existing temporary build folder and extension.
 rm -r build && rm -r *.so
 # run the python script and link sqlite.
-python3 cpp_setup.py build_ext --inplace 
+#python3 cpp_setup.py build_ext --inplace 
+python3 cpp_setup.py build_ext -larmadillo -lsqlite3 
