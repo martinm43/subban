6720bf3ea97d9303360eb6e32f33feff837c92fc
MA Miller <miller.m.43@gmail.com>
2024-09-28 20:52:39 -0400

    adding regular ext build

---
+++ src/predict/cython_mcss/ext_build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Delete existing temporary build folder and extension.
+rm -r build && rm -r *.so
+# run the python script and link sqlite.
+#python3 cpp_setup.py build_ext --inplace 
+python3 cpp_setup.py build_ext -larmadillo -lsqlite3
