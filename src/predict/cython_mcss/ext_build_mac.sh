6abeefaec1cee8c8c4bcb1e78c35019656b50374
MA Miller <miller.m.43@gmail.com>
2024-09-30 22:33:34 -0400

    adding in special files for mac build and special ext build

--- src/predict/cython_mcss/ext_build_mac.sh
+++ src/predict/cython_mcss/ext_build_mac.sh
@@ -2,4 +2,4 @@
 # Delete existing temporary build folder and extension.
 rm -r build && rm -r *.so
 # run the python script and link sqlite.
-python3 cpp_setup.py build_ext --inplace -larmadillo -lsqlite3
+python3 cpp_setup_mac.py build_ext --inplace -larmadillo -lsqlite3
